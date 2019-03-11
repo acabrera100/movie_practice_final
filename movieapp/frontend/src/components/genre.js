2963a6989bcf7bf37f7b0f64b83ea7352c9a0b71
Andres.C <andrescabrera@pursuit.org>
2019-03-11 16:29:37 -0400

    front-end setup nearly complete. Beginning back-end

--- movieapp/frontend/src/components/genre.js
+++ movieapp/frontend/src/components/genre.js
@@ -10,12 +10,35 @@ class Genre extends Component {
     return (
       <div className="movie-body">
         <div className="movie-info">
-        <h2>Page will render all movies by genre on mount. </h2>
-        <br />
-        <h4>Each movie should link to that movie's individual page </h4>
-        <br/>
-        <h4>Each movie item should include a 100x100px area of space for the movie's image. The movie image should not warp, but adjust its size to fit this space. Each movie item should have a 2px solid white border, with at least 15px of vertical space between each item.</h4>
-      </div>
+          <h4>Each movie should link to that movie's individual page </h4>
+          <br />
+          <h4>
+            Render all movies on mount, then when you make a selection clear the
+            page with movies only by that genre.
+          </h4>
+          <form>
+            <select className="SearchbyGenre">
+              <option value="AllMoviesOnMount" />
+              <option value="genre1">genre1</option>
+              <option value="genre2">genre2</option>
+              <option value="genre3">genre3</option>
+              <option value="genre4">genre4</option>
+            </select>
+            <input type="submit" value="Search by Genre" />
+          </form>
+          <div className="movieByGenre">
+          <ul>
+          <li>Movie Title#1</li>
+          <li><img className='movie-images'src=" https://i.pinimg.com/originals/da/06/5e/da065e13fc0f9d8ba57c8c05d9ee048c.jpg" alt='img placeholder'/></li>
+          <li>Average Rating: 100</li>
+          <br/>
+          <li>Movie Title#2</li>
+          <li><img className='movie-images'src=" https://i.pinimg.com/originals/39/c3/a2/39c3a28e321aa2d278a2b52b81bbf712.jpg" alt='img placeholder'/></li>
+          <li>Average Rating: 100</li>
+
+          </ul>
+          </div>
+        </div>
       </div>
     );
   }
--- movieapp/frontend/src/components/movies.js
+++ movieapp/frontend/src/components/movies.js
@@ -12,39 +12,28 @@ class Movies extends Component {
         <div className="movie-info">
           <form className="searchBar">
             <input placeholder="Search by Title" type="text" />
+            <br/>
             <input type="submit" />
-            <h2>Page will render all movies on mount. </h2>
-            <br />
-            <h4>
-              When a user enters part or all of a movie's title (not case
-              sensitive){" "}
-            </h4>
-            <br />
-            <h4>
-              {" "}
-              "Search" (submit), the list of movies should be filtered to only
-              the movies with titles that correspond to what the user was
-              searching for.
-            </h4>
-            <br />
-            <h4>
-              Each movie item should include a 100x100px area of space for the
-              movie's image. The movie image should not warp, but adjust its
-              size to fit this space.
-              <h4>Each movie item should have a 2px solid
-              white border, with at least 15px of vertical space between each
-              item.</h4>
-            </h4>
-            <br />
-            <h4>Each movie should link to that movie's individual page </h4>
+
           </form>
           <div className="movie-info">
+            <h3>This is what a single movie would look like</h3>
+            <h3>/movies/:id</h3>
+            <br/>
             <h3>Title</h3>
             <br />
             <h3>Genre</h3>
 
-            <img className='movie-images'src="https://i.pinimg.com/originals/da/06/5e/da065e13fc0f9d8ba57c8c05d9ee048c.jpg" alt='img placeholder'/>
-            <h3> Average Ratings</h3>
+            <img className='movie-images'src=" https://i.pinimg.com/originals/da/06/5e/da065e13fc0f9d8ba57c8c05d9ee048c.jpg" alt='img placeholder'/>
+            <h3> Average Ratings for each movie</h3>
+            <form>
+            <input type="radio" name="movieRating" value="1"/> 1
+            <input type="radio" name="movieRating" value="2"/> 2
+            <input type="radio" name="movieRating" value="3"/> 3
+            <input type="radio" name="movieRating" value="4"/> 4
+            <input type="radio" name="movieRating" value="5"/> 5
+            <input type='submit'/>
+            </form>
             <br />
             <form className="comments">
               <input type="text" />
@@ -56,6 +45,7 @@ class Movies extends Component {
               turnt upside down. Something seomthing something and my name is
               will smith and this is how I became to prince of bel-air
             </p>
+            <p className="comment">Comment#3</p>
           </div>
         </div>
       </div>
